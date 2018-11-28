729d7d1a815490b7998db84c2040342c51905976
angelhr28 <43622761+angelhr28@users.noreply.github.com>
2018-11-28 01:21:17 -0500

    gg acabe we

--- arboles.cpp
+++ arboles.cpp
@@ -1,164 +1,257 @@
 #include <iostream>
-#include <conio.h>
- 
-struct arbol
- {
- int dato;
- arbol *i,*d;
- }*elemento, *aux, *cabecera, *ant, *aux2, *ant2;
- 
-int dato;
- 
-int buscar(void);
-void insertar(void);
-void buscarmenmay(void);
-void buscarmaymen(void);
-void eliminar(void);
- 
-void main(void)
- {
- int y,opc;
- do
- {
- clrscr();
- y=10;
- gotoxy(10,y++);
- cout<<"0 - Salir";
- gotoxy(10,y++);
- cout<<"1 - Buscar";
- gotoxy(10,y++);
- cout<<"2 - Insertar";
- gotoxy(10,y);
- cout<<"3 - Borrar";
- gotoxy(10,y+=5);
- cout<<"Cual es su opcion: ";
- cin>>opc;
- switch(opc)
- {
- case 0: break;
- case 1: cout<<"\n\nDato a buscar: ";
- cin>>dato;
- if(buscar())
- cout<<"\n\nDato existe";
- else
- cout<<"\n\nDato inexistente";
- break;
- case 2: cout<<"\n\nDato a insertar: ";
- cin>>dato;
- insertar();
- cout<<"\n\nDato Insertado";
- break;
- case 3: cout<<"\n\nDato a borrar: ";
- cin>>dato;
- eliminar();
- break;
- default: cout<<"\n\nOpcion incorrecta";
- }
- if(opc) getch();
- }while(opc);
- }
- 
-int buscar(void)
- {
- if(!cabecera)
- {
- cout<<"No hay arbol";
- return(0);
- }
- ant=NULL;
- aux=cabecera;
- while(aux)
- {
- if (dato==aux->dato)
- return(1);
- else
- {
- ant=aux;
- if (dato>aux->dato)
- aux=aux->d;
- else
- aux=aux->i;
- }
- }
- return(0);
- }
- 
-void insertar(void)
- {
- if(!cabecera)
- {
- cabecera=new(arbol);
- cabecera->dato=dato;
- cabecera->d=NULL;
- cabecera->i=NULL;
- return;
- }
- if (!buscar())
- {
- aux=new(arbol);
- aux->dato=dato;
- aux->i=NULL;
- aux->d=NULL;
- if(dato>ant->dato)
- ant->d=aux;
- else
- ant->i=aux;
- }
- else
- cout<<"\n\nDato existente";
- }
- 
-void buscarmenmay(void)
- {
- aux2=aux->d;
- ant2=aux;
- while(aux2->i)
- {
- ant2=aux2;
- aux2=aux2->i;
- }
- aux->dato=aux2->dato;
- if(aux2->d)
- ant2->i=aux2->d;
- delete(aux2);
- ant2->d=NULL;
- }
- 
-void buscarmaymen(void)
- {
- aux2=aux->i;
- ant2=aux;
- while(aux2->d)
- {
- ant2=aux2;
- aux2=aux2->d;
- }
- aux->dato=aux2->dato;
- if(aux2->i)
- ant2->d=aux2->i;
- delete(aux2);
- ant2->i=NULL;
- }
- 
-void eliminar(void)
- {
- if(!buscar())
- {
- cout<<"\n\nElemento no encontrado.";
- return;
- }
- if(aux->d==NULL && aux->i==NULL)
- {
- if(ant->dato>dato)
- ant->i=NULL;
- else
- ant->d=NULL;
- delete(aux);
- }
- else
- if(aux->d!=NULL)
- buscarmenmay();
- else
- buscarmaymen();
- cout<<"\n\nElemento Borrado";
- }
+
+using namespace std;
+
+struct Nodo{
+     int Valor;
+     Nodo *Izq, *Der;
+};
+
+typedef struct Nodo *Abinario;
+Abinario crearNodo(int x)
+{
+     Abinario nuevoNodo = new(struct Nodo);
+     nuevoNodo->Valor = x;
+     nuevoNodo->Izq = NULL;
+     nuevoNodo->Der = NULL;
+
+     return nuevoNodo;
+}
+void insertar(Abinario &arbol, int x)
+{   // si el arbol esta vacio
+     if(arbol==NULL)
+     {
+           arbol = crearNodo(x);
+     }
+     else{ // si dato insertado es menor a la raiz
+          
+          if(x < arbol->Valor)
+        	insertar(arbol->Izq, x);
+          else // si dato insertado el mayor a la raiz
+	 	     if (x > arbol->Valor)
+          	   insertar(arbol->Der, x);
+	 }
+}
+
+int i=0;
+int a=100000;
+
+void reset(){
+	i=0;
+	a=100000;
+}
+
+void verArbol(Abinario arbol, int n)
+{
+     if(arbol==NULL){
+     	  return;
+	 }
+     else{
+     	verArbol(arbol->Der, n+1);
+     	for(int i=0; i<n; i++){
+         	cout<<"   ";
+    	 }
+     	cout<< arbol->Valor <<endl;
+    	verArbol(arbol->Izq, n+1);
+	 }   
+     
+}
+
+
+int max(Abinario arbol)
+{
+     if(arbol==NULL){
+     	  return 0;
+	 }
+     else if(i<arbol->Valor){
+     	 i=arbol->Valor;
+		max(arbol->Der);
+    	max(arbol->Izq);
+	 }   
+    return i;	
+}
+
+int min(Abinario arbol){
+	if(arbol ==NULL){
+		return 0;
+	}
+	else if(a>arbol->Valor){
+		a=arbol->Valor;
+		min(arbol->Der);
+    	min(arbol->Izq);	
+		}
+	return a;
+}
+
+bool busqueda(Abinario arbol,int bus){
+	if(arbol==NULL){
+		return false;
+	}
+	else if(arbol->Valor==bus){
+		return true;
+	}
+	else if(bus<arbol->Valor){
+		return busqueda(arbol->Izq,bus);
+	}
+	else{
+		return busqueda(arbol->Der,bus);
+	}
+}
+
+void preOrden(Abinario arbol){
+	if(arbol!=NULL){
+		cout<< arbol->Valor<<"	";
+		preOrden(arbol->Izq);
+		preOrden(arbol->Der);
+	}
+}
+void inOrden(Abinario arbol){
+	if(arbol!=NULL){
+		inOrden(arbol->Izq);
+		cout<<arbol->Valor<<"	";
+		inOrden(arbol->Der);
+	}
+}
+
+void posOrden(Abinario arbol){
+	if(arbol!=NULL){
+		posOrden(arbol->Izq);
+		posOrden(arbol->Der);
+		cout<<arbol->Valor<<"	";
+	}
+	
+}
+
+int conteo(Abinario arbol){
+	if(arbol!=NULL){
+		i++;
+		conteo(arbol->Izq);
+		conteo(arbol->Der);
+	}
+	return i;
+}
+
+int maxima(int a,int b){
+   if (a<b)
+      return b;
+   else
+      return a;
+}
+
+int alturaMaxima(Abinario arbol){
+   if (arbol != NULL){
+      if ((arbol->Der==NULL) && (arbol->Izq==NULL))
+         return 0;
+      else
+         return 1+maxima(alturaMaxima(arbol->Izq),alturaMaxima(arbol->Der));
+   }else
+      return 0;
+}
+
+void hojas(Abinario arbol){
+	if(arbol!=NULL){
+		
+		if((arbol->Izq==NULL) && (arbol->Der==NULL)){
+			cout<<arbol->Valor<<"	";
+		}
+		hojas(arbol->Izq);
+		hojas(arbol->Der);
+	}
+}
+
+void Menu()
+{
+	cout<<"1. Insertar "<<endl;
+	cout<<"2. Mostrar "<<endl;
+	cout<<"3. Numero Mayor"<<endl;
+	cout<<"4. Numero Menor"<<endl;
+	cout<<"5. Busqueda"<<endl;
+	cout<<"6. Pre-Orden"<<endl;
+	cout<<"7. In-Orden"<<endl;
+	cout<<"8. Pos-Orden"<<endl;
+	cout<<"9. Numero de elementos"<<endl;
+	cout<<"10. Altura y Nivel del arbol"<<endl;
+	cout<<"11. Hojas de un arbol"<<endl;
+	cout<<"Ingrese la opción que desea: ";
+}
+int main()
+{
+    Abinario arbol = NULL;   
+	int Opcion,Valor;
+
+	do {
+		Menu();
+		cin>>Opcion;
+		system("cls");
+		cout<<"-----------------------------OPERACION REALIZADA------------------------------"<<endl;
+		switch(Opcion)
+		{
+			case 1:
+				cout<<"Ingrese valor:"; 
+				cin>>Valor;
+				insertar( arbol, Valor);
+				break;
+			case 2:
+				cout << "\n Mostrando Arbol binario \n";
+				verArbol( arbol,0);
+				break;
+		
+			case 3: cout<<"	Numero mayor: ";
+				cout<<max(arbol)<<endl;
+				reset();
+				break;
+				
+		
+			case 4: cout<<"	Numero menor: ";
+				cout<<min(arbol)<<endl;
+				reset();
+				break;
+			case 5:
+				int bus;
+				cout<<"Digite el valor a buscar."<<endl;
+				cin>>bus;
+				if(busqueda(arbol,bus)==true){
+					cout<<"El valor	"<<bus<<"	se encuentra dentro del arbol."<<endl;
+				}
+				else {
+					cout<<"El numero no existe dentro del arreglo."<<endl;;
+				}
+				break;
+			case 6: 
+				preOrden(arbol);
+				cout<<endl;
+				break;
+			
+			case 7: 
+				inOrden(arbol);
+				cout<<endl;
+				break;
+				
+			case 8:
+				posOrden(arbol);
+				cout<<endl;
+				break;
+				
+			case 9:
+				cout<<conteo(arbol)<<endl;
+				reset ();	
+				break;
+			case 10:
+				cout<<"El nivel maxima del arbol es :"<<endl;
+				cout<<alturaMaxima(arbol)<<endl;
+				cout<<"La altura maxima del arbol es :"<<endl;
+				cout<<1+alturaMaxima(arbol)<<endl;
+				break;
+			case 11: 
+				cout<<"Los valores de las hojas encontradas son:	"<<endl;
+				hojas(arbol);
+				cout<<endl;
+				break;
+		}	
+		cout<<"----------------------VALORES DEL ARBOL-----------------------------"<<endl;
+		verArbol( arbol,0);
+		cout<<"---------------------------------------------------"<<endl;
+
+	} while (Opcion !=20);
+}
+
