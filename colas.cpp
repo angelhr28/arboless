d056fb5111a83a826751906d33a9ec01f93fd4dc
angelhr28 <43622761+angelhr28@users.noreply.github.com>
2018-11-28 22:31:38 -0500

    Add files via upload

--- colas.cpp
+++ colas.cpp
@@ -41,20 +41,21 @@ return (frente==NULL)? true : false;
 void busqueda(tcola frente){
 	int bus;
 	tcola aux=NULL;
-	tcola busq=NULL;
+//	tcola busq=NULL;
 	cout<<"indique el numero a buscar:	";
 	cin>>bus;
 	aux=frente;
 	while((aux!=NULL)&&(aux->Info!=bus)){
-		busq=aux;
+//		busq=aux;
 		aux=aux->sgte;
 	}
 	if(aux==NULL){
-		cout<<"el numero no existe.";
+		cout<<"el numero no existe dentro de la lista"<<endl;
 	}
-	else(busq==NULL){
-		
+	else if(aux->Info==bus){
+		cout<<"el numero	"<<aux->Info <<"	se encuentra dentro de la cola"<<endl;
 	}
+	
 }
 
 void eliminar(tcola &frente, tcola &fin, int &valor){
@@ -82,6 +83,27 @@ void mostrar(tcola frente, tcola fin, int valor){
 	frente=frente->sgte;
 	}
 }
+void eliminar_eleg(tcola &frente){
+	int bus;
+	tcola aux=NULL;
+	tcola delet=NULL;
+	cout<<"indique el numero a buscar:	";
+	cin>>bus;
+	delet=frente;
+	while((delet!=NULL)&&(delet->Info!=bus)){
+		aux=delet;
+		delet=delet->sgte;
+	}	
+	if(delet==NULL){
+		cout<<"el numero no se encuentra"<<endl;
+	}else if(aux==NULL){
+		frente=frente->sgte;
+		delete delet;
+	}else {
+		aux->sgte=delet->sgte;
+		delete delet;
+	}
+}
 
 int main (){
 	tcola frente=NULL;
@@ -104,7 +126,16 @@ int main (){
 			eliminar(frente,fin,dato);
 			break;
 	case 3: 
-		mostrar(frente,fin,dato);			
+		mostrar(frente,fin,dato);		
+		break;
+		
+	case 4: 
+		busqueda(frente);	
+		break;
+		
+	case 5:
+		eliminar_eleg(frente);
+		break;
 		}
 	}
 	return 0;
